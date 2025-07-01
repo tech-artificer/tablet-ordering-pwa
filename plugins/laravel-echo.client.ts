c36cdf9b6b7b9171bf942306e437c16af767f811
Rustom Pedales <pedales.rustom@gmail.com>
2025-07-01 17:05:14 +0800

    [Update]: laravel-reverb

--- plugins/laravel-echo.client.ts
+++ plugins/laravel-echo.client.ts
@@ -1,24 +1,23 @@
-// import Echo from 'laravel-echo';
-// import Pusher from 'pusher-js';
+import Echo from 'laravel-echo'
+import Pusher from 'pusher-js'
+export default defineNuxtPlugin(() => {
+    const config = useRuntimeConfig()
+    window.Pusher = Pusher;
 
-// export default defineNuxtPlugin(() => {
-//     const config = useRuntimeConfig()
-//     window.Pusher = Pusher;
-
-//     const echo = new Echo({
-//         broadcaster: 'reverb',
-//         key: config.public.NUXT_PUBLIC_REVERB_APP_KEY,
-//         wsHost: config.public.NUXT_PUBLIC_REVERB_HOST,
-//         wsPort: config.public.NUXT_PUBLIC_REVERB_PORT ?? 8081,
-//         wssPort: config.public.NUXT_PUBLIC_REVERB_PORT ?? 8081,
-//         forceTLS: false,
-//         disableStats: true,
-//         enabledTransports: ['ws'],
-//     });
-//     window.Echo = echo
-//     return {
-//         provide: {
-//             echo,
-//         }
-//     }
-// })
+    const echo = new Echo({
+        broadcaster: 'reverb',
+        key: config.public.NUXT_PUBLIC_REVERB_APP_KEY,
+        wsHost: config.public.NUXT_PUBLIC_REVERB_HOST,
+        wsPort: config.public.NUXT_PUBLIC_REVERB_PORT ?? 8081,
+        wssPort: config.public.NUXT_PUBLIC_REVERB_PORT ?? 8081,
+        forceTLS: false,
+        disableStats: true,
+        enabledTransports: ['ws'],
+    });
+    window.Echo = echo
+    return {
+        provide: {
+            echo,
+        }
+    }
+})
